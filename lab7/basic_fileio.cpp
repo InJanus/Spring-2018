0a7d40e87f18c3cbebb9da65292a2769f7c338b4
Brian Culberson <culberbn@mail.uc.edu>
2018-03-17 05:05:43 +0000

    final update of lab7

--- lab7/basic_fileio.cpp
+++ lab7/basic_fileio.cpp
@@ -1,7 +1,166 @@
-// Place your information here
+// Brian Culberson
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cmath>
+using namespace std;
+
+vector<int> getNums(string fileName){
+    vector<int> temp(1);
+    int line;
+    int count = 0;
+    ifstream myfile(fileName);
+    //myfile.open();
+    if (myfile.is_open()){
+        while(!myfile.eof()){       //you need a not here or it wont work
+            //cout << "Here!";
+            myfile >> line;
+            //cout << line << endl;
+            temp.at(count) = line;
+            count++;
+            temp.resize(count+1);
+        }
+        myfile.close();
+        temp.resize(count);
+        return temp;
+    }else{
+        cout << "Unable to open file" << endl;
+        return temp;
+    }
+}
+
+void sendToFile(vector<int> send, string fileName){
+    ofstream myfile(fileName);
+    if(myfile.is_open()){
+        for(unsigned int i = 0; i < send.size(); i++){
+            myfile << send.at(i) << "\n";
+        }
+        myfile.close();
+    } else {
+        cout << "Unable to open file" << endl;
+    }
+}
+
+bool isEven(int input){
+    return !(input%2);
+}
+
+bool isOdd(int input){
+    return (input%2);
+}
+
+bool isPrime(int input){
+    for(unsigned int i = 2; i < (input/4); i++){
+        if((input%i) == 0){
+            return false;
+        }
+    }
+    return true;
+    //6774
+}
+
+int max(vector<int> input){
+    int temp = input.at(0);
+    for(unsigned int i = 1; i < input.size(); i++){
+        if(input.at(i) > temp){
+            temp = input.at(i);
+        }
+    }
+    return temp;
+}
+
+int min(vector<int> input){
+    int temp = input.at(0);
+        for(unsigned int i = 1; i < input.size(); i++){
+        if(input.at(i) < temp){
+            temp = input.at(i);
+        }
+    }
+    return temp;
+}
+int sum(vector<int> input){
+    int temp = 0;
+    for(unsigned int i = 0; i < input.size(); i++){
+        temp = temp + input.at(i);
+    }
+    return temp;
+}
+
+float mean(vector<int> input){
+    return static_cast<double>(sum(input)/(double)input.size());
+}
+
+float stddev(vector <int> values) {
+    if(values.size() != 0){
+        float mymean = mean(values);
+        float total = 0;
+        for(int i = 0; i < values.size();i++){
+            total = total + pow(values.at(i) - mymean,2);
+        }
+        return pow((total/values.size()),0.5);
+    }
+    return 0.0;
+}
 
 int main() {
-  return 0;
+    string myfileName;
+    cout << "Please enter file name: " << endl;
+    getline(cin,myfileName);
+    vector<int> myNums = getNums(myfileName);
+    cout << "Elements in file: " << myNums.size() << endl;
+    int evencount = 0;
+    int count = 0;
+    vector<int> evens(1);
+    for(unsigned int i = 0; i < myNums.size(); i++){
+        evencount = evencount + isEven(myNums.at(i));
+        if(isEven(myNums.at(i))){
+            evens.at(count) = myNums.at(i);
+            //cout << evens.at(count) << endl;
+            count++;
+            evens.resize(count+1);
+        }
+    }
+    evens.resize(count);
+    sendToFile(evens, "evens.txt");
+    cout << "Even Elements: " << evencount << endl;
+    int oddcount = 0;
+    count = 0;
+    vector<int> odds(1);
+    for(unsigned int i = 0; i < myNums.size(); i++){
+        oddcount = oddcount + isOdd(myNums.at(i));
+        if(isOdd(myNums.at(i))){
+            odds.at(count) = myNums.at(i);
+            count++;
+            odds.resize(count+1);
+        }
+    }
+    odds.resize(count);
+    sendToFile(odds,"odds.txt");
+    cout << "Odd Elements: " << oddcount << endl;
+    
+    //prime numbers
+    int primecount = 0;
+    count = 0;
+    vector<int> prime(1);
+    for(unsigned int i = 0; i < myNums.size(); i++){
+        primecount = primecount + isPrime(myNums.at(i));
+        if(isPrime(myNums.at(i))){
+            prime.at(count) = myNums.at(i);
+            count++;
+            prime.resize(count+1);
+        }
+    }
+    prime.resize(count);
+    sendToFile(prime,"prime.txt");
+    cout << "Prime Elements: " << primecount << endl;
+    
+    cout << "Max Number: " << max(myNums) << endl;
+    cout << "Min Number: " << min(myNums) << endl;
+    cout << "Sum of Numbers: " << sum(myNums) << endl;
+    cout << "Mean Number: " << mean(myNums) << endl;
+    cout << "Standard Deviation: " << stddev(myNums) << endl;
+    
+    return 0;
 }
--- lab7/caesar.cpp
+++ lab7/caesar.cpp
@@ -1,8 +1,255 @@
 // Place your information here
 
 #include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+string Encode(string input,int value){ 
+    string temp;
+    char addchar;
+    for(unsigned int i = 0; i < input.size(); i++){
+        if(isalpha(input.at(i))){
+            if(islower(input.at(i))){
+                if(input.at(i)>(122-value)){
+                    addchar = ((input.at(i)-26)+value);
+                    temp = temp + addchar;
+                } else {
+                    addchar = input.at(i)+value;
+                    temp = temp + (addchar);
+                }
+            } else if(isupper(input.at(i))){
+                if(input.at(i)>(90-value)){
+                    addchar = ((input.at(i)-26)+value);
+                    temp = temp + addchar;
+                } else {
+                    addchar = input.at(i)+value;
+                    temp = temp + (addchar);
+                }
+            }
+        } else {
+            temp = temp + input.at(i);
+        }    
+    }
+    return temp;
+}
+
+string Decode(string input,int value){
+    string temp;
+    char addchar;
+    for(unsigned int i = 0; i < input.size(); i++){
+        if(isalpha(input.at(i))){
+            if(islower(input.at(i))){
+                if(input.at(i)<(97+value)){
+                    addchar = ((input.at(i)+26)-value);
+                    temp = temp + addchar;
+                } else {
+                    addchar = input.at(i)-value;
+                    temp = temp + (addchar);
+                }
+            } else if(isupper(input.at(i))){
+                if(input.at(i)<(65+value)){
+                    addchar = ((input.at(i)+26)-value);
+                    temp = temp + addchar;
+                } else {
+                    addchar = input.at(i)-value;
+                    temp = temp + (addchar);
+                }
+            }
+        } else {
+            temp = temp + input.at(i);
+        }    
+    }
+    return temp;
+}
+
+void fileWrite(string input, string fileName){
+    ofstream myfile(fileName);
+    if(myfile.is_open()){
+        myfile << input;
+        myfile.close();
+    } else {
+        cout << "Unable to open file" << endl;
+    }
+}
+
+string fileRead(string fileName){
+    string temp;
+    ifstream myfile(fileName);
+    if(myfile.is_open()){
+        while(!myfile.eof()){
+            getline(myfile,temp);
+            myfile.close();
+        }
+    } else {
+        cout << "Unable to open file" << endl;
+    }
+    return temp;
+}
+
+void outputToFile(string encodeMesage){
+    string fileName;
+    int input;
+    do{
+        cout << "Output to file? (1-Yes|0-No)" << endl;
+        cin >> input;
+        switch(input){
+            case 0:
+                break;
+            case 1:
+                input = 0;
+                cout << "File Name: ";
+                cin >> fileName;
+                fileWrite(encodeMesage,fileName);
+                cout << "DONE" << endl;
+                break;
+            default:
+                cout << "Invalid Option" << endl;
+                break;
+        }
+    }while((input!=0));
+}
+
+
+int getFactor(string temp){
+    int myreturn = 0;
+    for(unsigned int i = 0; i < temp.size();i++){
+        myreturn = myreturn + temp.at(i);
+        cout << (int)temp.at(i) << endl;
+    }
+    cout << myreturn << endl;
+    return ((myreturn % 23)+3);
+}
+void encodeMenu(){
+    int input;
+    string factor;
+    int factornum;
+    string fileName;
+    string mesage = "";
+    do{
+        cout << "Encode From Keyboard ---------- 1" << endl;
+        cout << "Encode From File -------------- 2" << endl;
+        cout << "Quit -------------------------- 0" << endl;
+        cin >> input;
+        switch(input){
+            case 1:{
+                cout << endl << "Please Type Mesage to Encode" << endl;
+                cin.ignore();
+                getline(cin,mesage);
+                cout << "Factor (Letter Format): ";
+                //cin.ignore();
+                getline(cin,factor);
+                factornum = getFactor(factor);
+                //cout << factornum << endl;
+                cout << endl;
+                string encodeMesage = Encode(mesage,factornum);
+                cout << "Endoded Mesage: " << encodeMesage;
+                cout << endl;
+                outputToFile(encodeMesage);
+                break;   //this is for case 1:
+            }
+            case 2:{
+                cout << "File Name: ";
+                cin >> fileName;
+                string read = fileRead(fileName);
+                cout << "Factor (Letter Format): ";
+                //cin.ignore();
+                getline(cin,factor);
+                factornum = getFactor(factor);
+                cout << endl;
+                cout << "Mesage: " << read << endl;
+                string encodeMesage = Encode(read,factornum);
+                cout << "Encoded Mesage: " << encodeMesage << endl;
+                outputToFile(encodeMesage);
+                break; //this is for case 2:
+            }
+            case 0:
+                break;
+            default:
+                cout << "Invalid Option" << endl;
+                break;
+        }
+        
+    }while(input != 0);
+}
+
+void decodeMenu(){
+    int input;
+    string factor;
+    int factornum;
+    string fileName;
+    string mesage = "";
+    do{
+        cout << "Decode From Keyboard ---------- 1" << endl;
+        cout << "Decode From File -------------- 2" << endl;
+        cout << "Quit -------------------------- 0" << endl;
+        cin >> input;
+        switch(input){
+            case 1:{
+                cout << endl << "Please Type Mesage to Decode" << endl;
+                cin.ignore();
+                getline(cin,mesage);
+                cout << "Factor (Letter Format): ";
+                //cin.ignore();
+                getline(cin,factor);
+                factornum = getFactor(factor);
+                cout << endl;
+                string encodeMesage = Decode(mesage,factornum);
+                cout << "Decode Mesage: " << encodeMesage;
+                cout << endl;
+                outputToFile(encodeMesage);
+                break;   //this is for case 1:
+            }
+            case 2:{
+                cout << "File Name: ";
+                cin >> fileName;
+                string read = fileRead(fileName);
+                cout << "Factor (Letter Format): ";
+                //cin.ignore();
+                getline(cin,factor);
+                factornum = getFactor(factor);
+                cout << endl;
+                cout << "Mesage: " << read << endl;
+                string decodeMesage = Decode(read,factornum);
+                cout << "Decoded Mesage: " << decodeMesage << endl;
+                outputToFile(decodeMesage);
+                break; //this is for case 2:
+            }
+            case 0:
+                break;
+            default:
+                cout << "Invalid Option" << endl;
+                break;
+        }
+        
+    }while(input != 0);
+    
+}
+
 
 int main() {
-  return 0;
+    int input;
+    string menu = "Caesar Cipher\nEncode String ---------- 1\nDecode String ---------- 2\nQuit ------------------- 0\n";
+    do{
+        cout << menu; 
+        cin >> input;
+        
+        switch(input){
+            case 1:
+                cout << endl;
+                encodeMenu();
+                break;
+            case 2:
+                cout << endl;
+                decodeMenu();
+                break;
+            case 0:
+                break;
+            default:
+                cout << "Invalid Option" << endl;
+        }
+    }while (input != 0);
+    
+    return 0;
 }
 
